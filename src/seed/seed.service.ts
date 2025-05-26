848b3cad3cf53391d75081b4a3896c0ec8feb521
gcruces95 <gabrielcrucesg@gmail.com>
2025-05-25 20:35:01 -0400

    perf: :zap: integrar módulo Pokemon en Seed para poblar la base de datos
    - Inyección del modelo de Pokémon en el servicio Seed - Permite crear registros de Pokémon en la base de datos

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
 import { PokeAPIResponse } from './interfaces/pokeApi-response.interface';
-
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
     const {data} = await this.axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon?limit=151');
@@ -13,10 +20,11 @@ export class SeedService {
     data.results.forEach(async ({name, url}) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({name, no});
+
+      await this.pokemonModel.create({name, no});
     });
 
-    return data.results;
+    return 'Seed executed';
   }
 
 }
