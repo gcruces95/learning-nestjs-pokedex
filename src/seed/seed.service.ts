6053d862d33f07eeaaf9ff0027eead00f626b253
gcruces95 <gabrielcrucesg@gmail.com>
2025-05-25 20:59:34 -0400

    feat: :sparkles: implementar AxiosAdapter y actualizar CommonModule para manejar solicitudes HTTP
    - Se agrega AxiosAdapter para gestionar solicitudes HTTP.
    - Se actualiza CommonModule para incluir AxiosAdapter como proveedor y exportarlo.
    - Se crea la interfaz HttpAdapter para definir el contrato de adaptadores HTTP.
    - Se modifica SeedService para utilizar AxiosAdapter en lugar de axios directamente.

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { PokeAPIResponse } from './interfaces/pokeApi-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+
 @Injectable()
 export class SeedService {
-
-  private readonly axios: AxiosInstance = axios;
 
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({});
 
-    const {data} = await this.axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const data = await this.http.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon?limit=151');
 
     const pokemonToInsert: {name: string, no: number}[] = [];
 
